f01b9f844ad278aea538a261227a624ff0753781
Armin RAUSCHENBERGER <armin.rauschenberger@uni.lu>
2020-11-02 16:51:51 +0100

    vignette

--- R/functions.R
+++ R/functions.R
@@ -81,10 +81,12 @@
 #' \code{\link{cv.joinet}}, vignette
 #' 
 #' @examples
+#' if(!grepl('SunOS',Sys.info()['sysname'])){
 #' n <- 50; p <- 100; q <- 3
 #' X <- matrix(rnorm(n*p),nrow=n,ncol=p)
 #' Y <- replicate(n=q,expr=rnorm(n=n,mean=rowSums(X[,1:5])))
 #' object <- joinet(Y=Y,X=X)
+#' }
 #' 
 #' \dontrun{
 #' browseVignettes("joinet") # further examples}
