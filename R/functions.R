5e16932b33ed6c3b48271b84f45efd6927609539
Armin RAUSCHENBERGER <armin.rauschenberger@uni.lu>
2020-11-03 08:59:47 +0100

    vignette

--- R/functions.R
+++ R/functions.R
@@ -85,12 +85,15 @@
 #' 
 #' @examples
 #' \dontshow{
-#' if(!grepl('SunOS',Sys.info()['sysname'])){joinet <- function(...){}}
+#' if(!grepl('SunOS',Sys.info()['sysname'])){
 #' }
 #' n <- 50; p <- 100; q <- 3
 #' X <- matrix(rnorm(n*p),nrow=n,ncol=p)
 #' Y <- replicate(n=q,expr=rnorm(n=n,mean=rowSums(X[,1:5])))
 #' object <- joinet(Y=Y,X=X)
+#' \dontshow{
+#' }
+#' }
 #' 
 #' \dontrun{
 #' browseVignettes("joinet") # further examples}
