a3ad08dc75e5a62c267c45a8a612eb1b539214b9
Armin Rauschenberger <armin.rauschenberger@uni.lu>
2021-08-05 17:52:55 +0200

    reference

--- R/pkgname.R
+++ R/pkgname.R
@@ -24,9 +24,10 @@
 #' to open the vignette.
 #' 
 #' @references
-#' Armin Rauschenberger and Enrico Glaab (2020).
-#' "Predicting correlated outcomes from molecular data".
-#' \emph{Manuscript in preparation}.
+#' Armin Rauschenberger, Enrico Glaab (2021)
+#' "Predicting correlated outcomes from molecular data"
+#' \emph{Bioinformatics}. btab576
+#' \url{https://doi.org/10.1093/bioinformatics/btab576}
 #' 
 #' \email{armin.rauschenberger@uni.lu}
 #'
